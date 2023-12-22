7655dd7b1eb99b9b06d5489a3761f6a2028141a9
Vic Rubba <crazedVic@users.noreply.github.com>
2023-12-22 12:03:10 -0700

    cleaned up routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/', function (Request $request, Response $response) {
+    return response()->json(['Laravel API Version' => App::version()]);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
